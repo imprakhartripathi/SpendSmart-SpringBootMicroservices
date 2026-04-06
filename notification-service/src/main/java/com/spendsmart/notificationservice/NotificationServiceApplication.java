aea86584d9a2c5a9160eb7e019c405607529f260
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2026-04-06 12:25:44 +0530

    initial commit (inc. setup of microservices)

---
+++ notification-service/src/main/java/com/spendsmart/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spendsmart.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
---
+++ recurring-service/src/main/java/com/spendsmart/recurringservice/RecurringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spendsmart.recurringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecurringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecurringServiceApplication.class, args);
+    }
+
+}
